c180bf53eea6b9210bbe26ccd0992b0955d2e5d5
CarlosMafraNeto <c.a.m.o.neto@gmail.com>
2022-07-12 18:43:48 -0300

    Última versão do Light and Dark Mode

---
+++ script.js
@@ -0,0 +1,11 @@
+const BOTAO = document.getElementById('btn')
+const TEXTO = document.getElementById('pageTitle')
+const Body = document.getElementsByTagName('body')[0]
+
+BOTAO.addEventListener('click', changeMode)
+
+function changeMode() {
+  console.log('SIm, está apertando')
+}
+
+function changeText() {}
